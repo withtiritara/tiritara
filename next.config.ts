a9e42d8b6da23619ae9dfcf653712ae1810b0fef
Tiri Tara <withtiritara@gmail.com>
2025-09-24 10:20:38 +0000

    error for     {       "id": "south_goa_hero",       "description": "A b

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
